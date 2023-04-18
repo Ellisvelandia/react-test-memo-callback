287f0b00cdf965a525af8e8a7b9f216b8d203690
Ellisvelandia <eyis619l@gmail.com>
2023-04-18 18:22:31 -0500

    init project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <form action="" className="form">
+          <input type="text" placeholder="Avengers, Star Wars, The matrix..." />
+          <button type="submit">Buscar</button>
+        </form>
+      </header>
+       main
+    </div>
+  );
+};
+
+export default App;
