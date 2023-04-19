204ecafd35a4aba4f01c18d564fb7b08395e7b77
Ellisvelandia <eyis619l@gmail.com>
2023-04-19 00:38:01 -0500

    responsive desgign grids movies & writting datas

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import React from "react";
+import responseMovies from "./mocks/with-results.json";
 import "./App.css";
 
 const App = () => {
+  const movies = responseMovies.Search;
+  const hasMovies = movies?.length > 0;
   return (
     <div className="page">
       <header>
         <form className="form">
-          <input type="text" placeholder="Search Avengers, Star Wars, The matrix..." />
+          <input
+            type="text"
+            placeholder="Search Avengers, Star Wars, The matrix..."
+          />
           <button type="submit">Buscar</button>
         </form>
       </header>
-       <main>
-        Aqui iran los resultados
-       </main>
+      <main>
+        {hasMovies ? (
+          <ul className="movies">
+            {movies.map((movie) => (
+              <li key={movie.imdbID}>
+                <h2>{movie.Title}</h2>
+                <p>{movie.Year}</p>
+                <img src={movie.Poster} alt={movie.Title} loading="lazy" />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div></div>
+        )}
+      </main>
     </div>
   );
 };
